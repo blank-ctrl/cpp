226887156089d3dc07a2a2abf72d35ff3d60e7ad
blank-ctrl <blanklikemybank@gmail.com>
2024-01-06 21:03:27 +0100

    u

---
+++ tests/tests.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+    int num1, num2, sum;
+
+    std::cout << "Num1: \n";
+    std::cin >> num1;
+
+    std::cout << "Num2: \n";
+    std::cin >> num2; 
+
+    sum = num1 + num2;
+
+    std::cout << sum;
+
+
+    return 0;
+}
